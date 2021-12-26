cbf516a4e0fbe39fb78a740fb22fd2f8048e152a
Adarsh D <adarshd181998@gmail.com>
2021-12-26 21:37:50 +0530

    C program working

--- src/main/java/com/clowderin/Codingile/controllers/CodingController.java
+++ src/main/java/com/clowderin/Codingile/controllers/CodingController.java
@@ -1,14 +1,54 @@
 package com.clowderin.Codingile.controllers;
 
 import com.clowderin.Codingile.models.request.CompileRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
+
 @RestController
-@RequestMapping
+@RequestMapping("/compile")
+@Slf4j
 public class CodingController {
-    @GetMapping("/python")
-    public ResponseBody compilePython(CompileRequest compileRequest){
-        return null;
+//    @GetMapping("/python")
+//    public void compilePython(CompileRequest compileRequest){
+//        return null;
+//    }
+
+    @GetMapping("/gcc")
+    public void compileGcc(CompileRequest compileRequest) throws IOException {
+        String programId = java.util.UUID.randomUUID().toString();
+        File tempDirectory = new File(System.getProperty("user.dir"));
+        File programFile = new File(tempDirectory+"/temp", programId+".c");
+        FileWriter myWriter = new FileWriter(programFile);
+        myWriter.write(compileRequest.getProgramBody());
+        myWriter.close();
+
+        File inputFile = new File(tempDirectory+"/temp",programId+"input");
+        FileWriter inputWriter = new FileWriter(inputFile);
+        inputWriter.write(compileRequest.getInput());
+        inputWriter.close();
+
+        String scriptPath = System.getProperty("user.dir") + "/scripts";
+        String programPath = programFile.getPath();
+
+        ProcessBuilder builder = new ProcessBuilder("/bin/bash",scriptPath+"/gcc_runner.sh",programPath,inputFile.toString(),programId);
+        Process process = builder.start();
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(process.getInputStream()));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+        while ( (line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(System.getProperty("line.separator"));
+        }
+        String result = stringBuilder.toString();
+        inputFile.delete();
+        programFile.delete();
+
+        System.out.println("Result: " + result);
+
     }
 }
 
--- src/main/java/com/clowderin/Codingile/models/request/CompileRequest.java
+++ src/main/java/com/clowderin/Codingile/models/request/CompileRequest.java
@@ -3,10 +3,13 @@ package com.clowderin.Codingile.models.request;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class CompileRequest {
     private String languageCode;
     private String programBody;
+    private String input;
 }
